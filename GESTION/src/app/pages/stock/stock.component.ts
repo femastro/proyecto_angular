a1825be32154204391ecdd10ea7bcf0ce2cfefb9
femastro <fernando.mastropietro@gmail.com>
2020-11-10 02:00:02 -0300

    --amend

--- GESTION/src/app/pages/stock/stock.component.ts
+++ GESTION/src/app/pages/stock/stock.component.ts
@@ -1,8 +1,5 @@
-import { Observable } from 'rxjs';
 import { StockService } from './../services/stock.service';
 import { Component, OnInit } from '@angular/core';
-import { Prod } from 'src/app/shared/interfaces/prod.interface';
-
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
